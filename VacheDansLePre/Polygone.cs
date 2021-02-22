ae11884e36319c2bf1ee924c2dfb2bf955fbaff1
Henri Vanlemmens <hb.vanlemmens@orange.fr>
2021-02-22 15:13:48 +0100

    Calcul centre de gravité et l'aire + tests affichages

--- VacheDansLePre/Polygone.cs
+++ VacheDansLePre/Polygone.cs
@@ -22,12 +22,37 @@ namespace VacheDansLePre
         }
         public void CalculCentreGravite()
         {
-            
+            float somme_x = 0;
+            float somme_y = 0;
+            for (byte i = 0; i < listePiquet.Count() - 1; i++)
+            {
+                somme_x = somme_x + ((listePiquet[i].xP + listePiquet[i + 1].xP)*(listePiquet[i].xP * listePiquet[i + 1].yP - listePiquet[i + 1].xP * listePiquet[i].yP));
+                if (i == listePiquet.Count() - 2)
+                    somme_x = somme_x + ((listePiquet[i+1].xP + listePiquet[0].xP )* (listePiquet[i+1].xP * listePiquet[0].yP - listePiquet[0].xP * listePiquet[i+1].yP));
+            }
+
+            for (byte i = 0; i < listePiquet.Count() - 1; i++)
+            {
+                somme_y = somme_y + ((listePiquet[i].yP + listePiquet[i + 1].yP) * (listePiquet[i].xP * listePiquet[i + 1].yP - listePiquet[i + 1].xP * listePiquet[i].yP));
+                if (i == listePiquet.Count() - 2)
+                    somme_y = somme_y + ((listePiquet[i+1].yP + listePiquet[0].yP) * (listePiquet[i+1].xP * listePiquet[0].yP - listePiquet[0].xP * listePiquet[i+1].yP));
+            }
+
+            centreGravite.xG = (float)(1 / (6 * airePolygone)*somme_x);
+            centreGravite.yG = (float)(1 / (6 * airePolygone)*somme_y);
         } 
          
         public void CalculAire()
         {
-            
+            float somme = 0;
+            for (byte i = 0; i < listePiquet.Count() - 1; i++)
+            {
+                somme = somme + (listePiquet[i].xP * listePiquet[i + 1].yP - listePiquet[i + 1].xP * listePiquet[i].yP);
+                if (i == listePiquet.Count()-2)
+                    somme = somme + (listePiquet[i+1].xP * listePiquet[0].yP - listePiquet[0].xP * listePiquet[i+1].yP);
+            }
+
+            airePolygone = (float)0.5 * somme;
         }
 
         
--- VacheDansLePre/Program.cs
+++ VacheDansLePre/Program.cs
@@ -33,7 +33,31 @@ namespace VacheDansLePre
             } while (!valid);
 
 
-          
+            //.Replace(".", System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+
+            while (nb_piquet!=0)
+            {
+                Console.WriteLine("Saisir le piquet {0} ", nb_piquet);
+
+                Console.WriteLine("X");
+                p_x = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Y");
+                p_y = Convert.ToSingle(Console.ReadLine());
+
+                Enclos.listePiquet.Add(new Piquet(p_x, p_y));
+                nb_piquet--;
+            }
+            Enclos.CalculAire();
+            Console.WriteLine("Aire = {0}", Enclos.airePolygone);
+            Enclos.airePolygone = Math.Abs(Enclos.airePolygone);
+            Console.WriteLine("Aire = {0} valeur absolue\n", Enclos.airePolygone);
+
+            Enclos.CalculCentreGravite();
+            Console.WriteLine("Centre de gravité = abscisse : {0}, ordonnée : {1} ", Enclos.centreGravite.xG, Enclos.centreGravite.yG);
+
+            Console.ReadLine();
+
         }
     }
 }
