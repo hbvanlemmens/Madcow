d96bdcf942d7c69dc6f5eff718161a471735f781
Henri Vanlemmens <hb.vanlemmens@orange.fr>
2021-02-22 13:49:36 +0100

    Ajoutez des fichiers projet.

---
+++ VacheDansLePre/CentreGravite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacheDansLePre
+{
+    class CentreGravite
+    {
+        public float xG { get; set; }
+        public float yG { get; set; }
+
+        public CentreGravite(float p_xG, float p_yG)
+        {
+            xG = p_xG;
+            yG = p_yG;
+        }
+
+        public bool AppartenancePointPolygone(Polygone polygone)
+        {
+            return true;
+        }
+    }
+}
---
+++ VacheDansLePre/Polygone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacheDansLePre
+{
+    class Polygone
+    {
+        public List<Piquet> listePiquet = new List<Piquet>();
+
+        public float airePolygone { get; set; }
+        public CentreGravite centreGravite = new CentreGravite(0,0);
+
+        public Polygone(List<Piquet> p_listePiquet)
+        {
+            listePiquet = p_listePiquet;
+            airePolygone = 0.0f;
+            centreGravite.xG = 0.0f;
+            centreGravite.yG = 0.0f;
+        }
+        public void CalculCentreGravite()
+        {
+            
+        } 
+         
+        public void CalculAire()
+        {
+            
+        }
+
+        
+
+    }
+}
