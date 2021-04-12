70ae71cb6f4d133668a757fe0da3a94c9ab1d02e
GabrielGruit <ggabri432@gmail.com>
2021-04-12 16:26:14 +0200

    Ajout de commentaires dans vecteur

--- VacheDansLePre/Vecteur.cs
+++ VacheDansLePre/Vecteur.cs
@@ -11,18 +11,23 @@ namespace VacheDansLePre
         public float xV { get; set; }
         public float yV { get; set; }
 
+        //constructeur de vecteur
         public Vecteur(float p_xV, float p_yV)
         {
             xV = p_xV;
             yV = p_yV;
         }
 
+        //----------méthodes---------
+
+        //Calcul des coordonées du vecteur 
         public void CalculCoordVecteur(CentreGravite centreGravite, Piquet p_piquet2)
         {
             xV = p_piquet2.xP - centreGravite.xG;
             yV = p_piquet2.yP - centreGravite.yG;
         }
 
+        //Calcul d'un produit scalaire à partir de deux vecteurs
         public float CalculProdScalaire(Vecteur p_v1, Vecteur p_v2)
         {
             float produit;
@@ -30,10 +35,11 @@ namespace VacheDansLePre
             return produit;
         }
 
+        //Calcul de norme de vecteur
         public float CalculNormeVecteur(Vecteur p_v1)
         {
             float norme;
-            norme = (float)Math.Sqrt(Math.Pow(p_v1.xV, 2) + Math.Pow(p_v1.yV, 2));
+            norme = (float)Math.Sqrt(Math.Pow(p_v1.xV, 2) + Math.Pow(p_v1.yV, 2));//cast du calcul pour gérer le type
             return norme;
         }
     }
